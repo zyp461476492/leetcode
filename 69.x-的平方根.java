5f53b4838638ed638ff34b73605429c6eec3211d
zhangyp <13150337230@163.com>
2019-10-15 16:05:16 +0800

    add 69 answer

---
+++ 69.x-的平方根.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=java
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int a) {
+        long x = a;
+        while (x * x > a) {
+            x = (x + a / x) / 2;
+        }
+        return (int) x;
+    }
+}
+// @lc code=end
+
