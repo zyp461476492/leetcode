f89d5ee7982f1b410ffc44e2c89be28344ce69ad
zhangyp <13150337230@163.com>
2019-10-17 15:06:38 +0800

    add 104 answer

---
+++ 104.二叉树的最大深度.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=java
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+// @lc code=end
+
