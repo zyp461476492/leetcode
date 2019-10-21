4c74f4f04c74b5e185ee39d392378284a9a98f60
zhangyp <13150337230@163.com>
2019-10-21 14:44:28 +0800

    add 119 answer

---
+++ 119.杨辉三角-ii.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=java
+ *
+ * [119] 杨辉三角 II
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> cur = new ArrayList<>();
+        cur.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                cur.set(j, cur.get(j - 1) + cur.get(j));
+            }
+            cur.add(1);// 补上每层的最后一个 1
+        }
+        return cur;
+
+    }
+}
+// @lc code=end
