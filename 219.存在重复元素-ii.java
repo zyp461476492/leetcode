ad14da9ed84a31b065e119ca3465d983ca462923
zhangyp <13150337230@163.com>
2019-11-04 13:53:37 +0800

    add 219 answer

--- 219.存在重复元素-ii.java
+++ 219.存在重复元素-ii.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 // @lc code=start
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // 核心思想就是要维持K个窗口，然后去看是否存在一样的
+        // 核心思想就是要维持K个窗口，然后去看是否存在一样的  
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; ++i) {
             if (set.contains(nums[i]))
