f11ddfe95ec1a259eb54d9de58e5bb9d494bdcf8
zhangyp <13150337230@163.com>
2019-11-13 11:50:45 +0800

    add 303,326.3 answer

---
+++ 303.区域和检索-数组不可变.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=java
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+class NumArray {
+    int[] sum;
+    public NumArray(int[] nums) {
+        this.sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        return this.sum[j + 1] - this.sum[i];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+// @lc code=end
+
