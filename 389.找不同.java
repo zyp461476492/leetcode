325a8afdef4ec568e7d98a901c4bd3c470b08135
zhangyp <13150337230@163.com>
2019-11-19 20:04:48 +0800

    add 389 answer

---
+++ 389.找不同.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=389 lang=java
+ *
+ * [389] 找不同
+ */
+
+// @lc code=start
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] bucket = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            bucket[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            bucket[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] < 0) {
+                char c = (char)('a' + i);
+                return c;
+            }
+        }
+        return ' ';
+    }
+}
+// @lc code=end
+
