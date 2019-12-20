3979a5e6f4cb50dab7aed26ed48bb850c59c78d8
zhangyp <13150337230@163.com>
2019-12-20 14:44:52 +0800

    add 637 answer

---
+++ 637.二叉树的层平均值.java
@@ -0,0 +1,35 @@
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int count = queue.size(); // 当前层的个数
+            double sum = 0.0;
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            res.add(sum / count);
+        }
+        return res;
+    }
+}
+// @lc code=end
