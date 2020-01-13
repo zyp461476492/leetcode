e3ab962ed84cd8428c076e8e4f55612128281e01
zhangyp <13150337230@163.com>
2020-01-13 14:34:17 +0800

    add 347 answer

---
+++ 347.前-k-个高频元素.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode.cn id=347 lang=java
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        for (int n : nums) {
+            if (map.containsKey(n)) {
+                map.put(n, map.get(n) + 1);
+            } else {
+                map.put(n, 1);
+            }
+        }
+        
+        //桶排序
+        //将频率作为数组下标，对于出现频率不同的数字集合，存入对应的数组下标
+        List<Integer>[] list = new List[nums.length+1];
+        for(int key : map.keySet()){
+            // 获取出现的次数作为下标
+            int i = map.get(key);
+            if(list[i] == null){
+               list[i] = new ArrayList();
+            } 
+            list[i].add(key);
+        }
+        
+        // 倒序遍历数组获取出现顺序从大到小的排列
+        // 这里排序是因为出现次数最大就是 nums.length 次
+        // 所以从 list.length - 1 开始就是从大到小进行排序
+        for(int i = list.length - 1;i >= 0 && res.size() < k;i--){
+            if(list[i] == null) continue;
+            res.addAll(list[i]);
+        }
+        return res;
+        
+        
+    }
+}
+// @lc code=end
+
