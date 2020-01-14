4142631f5b1446cd10124d708b66f8613d2bd43c
zhangyp <13150337230@163.com>
2020-01-14 22:16:37 +0800

    fish commit

---
+++ 324.摆动排序-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=324 lang=java
+ *
+ * [324] 摆动排序 II
+ */
+
+// @lc code=start
+class Solution {
+    public void wiggleSort(int[] nums) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+}
+// @lc code=end
+
